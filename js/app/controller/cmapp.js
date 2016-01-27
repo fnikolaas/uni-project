c600c42d73801fce4b21c45732d76f4b6a0447d5
Christian Kiesewetter <chrkiesew@gmail.com>
2016-01-27 12:45:52 +0100

    still initial changes for grid refactoring

--- js/app/controller/cmapp.js
+++ js/app/controller/cmapp.js
@@ -10,6 +10,11 @@ CmApp.prototype.eventOnBtnClick = function(btn) {
     case 'b_search':
         this.searchProducts();
         break;
+    /* ---------- shopping cart ----------- */
+    case 'add_to_cart':
+        sc.addItem();
+        break;
+    /* ------------------------------------ */
     default:
         // do nothing
         break;
@@ -19,28 +24,28 @@ CmApp.prototype.eventOnBtnClick = function(btn) {
 CmApp.prototype.searchProducts = function() {
     var cnt = Util.getFieldsContent();
     var me = this;
-    
+
     var cb = function(ret) {
         me.displayData(ret);
     };
-    
+
     Util.doRequest({service: 'search_prod', search: cnt.t_search}, cb);
 }
 
 CmApp.prototype.displayData = function(data) {
     var grid = new Grid;
-    
+
     var colConfig = [
         {key: 'productName', desc: 'Name', width: 350},
         {key: 'productScale', desc: 'Scale', width: 40},
         {key: 'buyPrice', desc: 'Price', width: 70}
     ];
-    
+
     grid.create(colConfig, 'result-grid-cnt', data);
-    
+
     grid.eventOnRowSelect = function(row) {
         Util.setFieldsContent(row.data);
     };
-    
+
     document.getElementById('count').innerHTML = 'results: ' + data.length;
 }
--- js/jsbase/util.js
+++ js/jsbase/util.js
@@ -4,53 +4,53 @@ function Util() {
 Util.prototype.getElementsByClassName = function(className) {
     var nodeList = {'DIV': 1, 'SPAN': 1, 'INPUT': 1};
     var ret = [];
-    
+
     for (var j in nodeList) {
         var nodes = document.getElementsByTagName(j);
         var node  = null;
 
         for (var i = 0, l = nodes.length; i < l; i++) {
             node = nodes[i];
-            
+
             var cls = node.className.split(' ');
             var classes = this.arrayToHash(cls);
-            
+
             if (className in classes) {
                 ret.push(node);
             }
         }
     }
-    
+
     return ret;
 }
 
 Util.prototype.arrayToHash = function(arr) {
     var o = {};
-    
+
     for (var i = 0, l = arr.length; i < l; i++) {
         o[arr[i]] = 1;
     }
-    
+
     return o;
 }
 
 Util.prototype.getFieldsContent = function() {
     var fields = document.getElementsByTagName('INPUT');
     var ret = {};
-    
+
     for (var i = 0, n; n = fields[i]; i++) {
         if (n.type == 'text') {
             ret[n.id] = n.value;
         }
     }
-    
+
     return ret;
 }
 
 Util.prototype.setFieldsContent = function(data) {
     for (var i in data) {
         var n = document.getElementById(i);
-        
+
         if (n) {
             n.value = data[i];
         }
@@ -63,9 +63,9 @@ Util.prototype.doRequest = function(params, cb) {
     var reqParams = this.addParams(params);
 
     request.open('POST', baseUrl, true);
-    
+
     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    
+
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status === 200) {
             cb(eval(request.responseText));
@@ -77,20 +77,41 @@ Util.prototype.doRequest = function(params, cb) {
 
 Util.prototype.addParams = function(params) {
     var paramsStrg = '';
-    
+
     for (var i in params) {
         paramsStrg += paramsStrg == '' ? '' : '&';
-        
+
         if (i == 'service') {
             paramsStrg += i + '=' + params[i];
         } else {
             paramsStrg += 'params[' + i + ']=' + params[i];
         }
-        
+
     }
-    
+
     return encodeURI(paramsStrg);
 }
+/* ------------- Shopping Cart ------------------- */
+Util.prototype.displayShoppingCart = function(data)
+{
+  var grid = new Grid;
 
+  var colConfig = [
+      {key: 'productName', desc: 'Name', width: 280},
+      {key: 'productScale', desc: 'Scale', width: 40},
+      {key: 'buyPrice', desc: 'Price', width: 70},
+      {key: 'count', desc: 'qty', width: 50},
 
+      {
+        desc: '',
+        width: 20,
+        reference: 'productCode',
+        methodDisplay: '<b>x</b>',
+        method: (function(r_prodCode){
+          sc.removeItem(r_prodCode);
+          Util.displayShoppingCart(sc.items);
+        })
+      }
+  ];
+/* ----------------------------------------------- */
 var Util = new Util;
